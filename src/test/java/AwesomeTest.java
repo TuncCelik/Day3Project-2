015a2e29bb3762b5a18c7bd0acb9cd40c7320f60
Erhan <erhanilgar@gmail.com>
2020-10-06 20:50:34 +0100

    last

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -1,2 +1,6 @@
 public class AwesomeTest {
+    //red means it's untracked
+    //green means it's under staging area
+    //grey in .idea folder means it's ignored
+    //color normal , it means commited
 }
